8455aed5148a2e0f465af3667433d09e9da06f13
aca14bfm <bfmillward1@sheffield.ac.uk>
2015-04-17 00:51:37 +0100

    Added get and set element functionality to NMatrix.

--- math/NMatrix.java
+++ math/NMatrix.java
@@ -1,10 +1,34 @@
 package classes.math;
 public class NMatrix {
 	
-	double[][] elements;
+	private double[][] elements;// [row][column]
+	private int rows, columns;
 	
-	public NMatrix (int width, int height)
+	public NMatrix (int rows, int columns)
 	{
-		elements = new double[width][height];
+		elements = new double[rows][columns];
+		this.rows = rows;
+		this.columns = columns;
+	}
+	
+	// Getters
+	public double getElement (int i, int j)
+	{
+		if(i>= 0 && i <=rows && j>= 0 && j<=columns)
+		{
+			return(elements[i][j]);
+		}else{
+			System.out.println("ERROR: An element index of [" + i + "," + j + "] is out of bounds, returning NaN.");
+			return(Double.NaN);
+		}
+	}
+	
+	// Setters
+	public void setElement (int i, int j, double value)
+	{
+		if(i>= 0 && i <=rows && j>= 0 && j<=columns)
+		{
+			elements[i][j] = value;
+		}
 	}
 }
