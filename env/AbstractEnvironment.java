5aaadb85d0251a92cbdf287b7866752909a36030
aca14bfm <bfmillward1@sheffield.ac.uk>
2015-07-22 03:46:33 +0100

    Started work on AbstractEnvironment, TODO: main method

--- env/AbstractEnvironment.java
+++ env/AbstractEnvironment.java
@@ -1,23 +1,36 @@
 package classes.env;
 
+import classes.env.DistancedHit;
+import classes.math.StaticPoint;
+
 public abstract class AbstractEnvironment {
 		
-		//public ArrayList<AbstractAgent> agents = new ArrayList<AbstractAgent>();
-		public ArrayList<BoundedEntity> entities = new ArrayList<AbstractEntity>();
+		public ArrayList<AbstractEntity> entities = new ArrayList<AbstractEntity>();
 		
-		public double distanceScan(double x1, double y1, double x2, double y2)
+		public DistancedHit hitScan(double x1, double y1, double x2, double y2)
+		{
+			DistancedHit closest = new DistancedHit(false, x2, y2, Math.hypot(x1-x2,y1-y2));
+			for(AbstractEntity e : entities)
+			{
+				DistancedHit h = e.hitScan(x1,y1,x2,y2);
+				if(h.getDistance() > closest.getDistance())
+					closest = h;
+			}
+			return closest;
+		}
+		public double DistanceMeasure(double x1, double y1, double x2, double y2)
 		{
 			double closest = Math.POSITIVE_INFINITY;
 			for(AbstractEntity e : entities)
 			{
-				Hit h = e.hitScan(x1,y1,x2,y2);
+				DistancedHit h = e.hitScan(x1,y1,x2,y2);
 				if(h.hadContact())
 					closest = Math.min(closest, h.getDistance());
 			}
 			return closest;
 		}
-		public double distanceScan(Point p1, Point p2)
+		public double DistanceMeasure(StaticPoint p1, StaticPoint p2)
 		{
-			return distanceScan(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+			return DistanceMeasure(p1.getX(), p1.getY(), p2.getX(), p2.getY());
 		}
 }
