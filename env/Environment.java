386baf961e1a677e1c17b3d0cc5775d9d9509173
aca14bfm <bfmillward1@sheffield.ac.uk>
2015-07-23 03:27:37 +0100

    Environment class finished! Roll on the learning autonomous agents! WOOO!

--- env/Environment.java
+++ env/Environment.java
@@ -1,11 +1,13 @@
 package classes.env;
 
-import classes.env.AbstractEntity;
-import classes.env.DistancedHit;
+import classes.env.*;
 import classes.math.StaticPoint;
 import java.awt.Graphics2D;
+import java.util.*;
+import classes.graphics.SimpleDisplay;
+import java.awt.Color;
 
-public class Environment {
+public class Environment {//TODO: Make this extend AbstractEntity? Note that entities would have to be private for this to work in order to get the corners and width
 		
 		public ArrayList<AbstractEntity> entities; //Perhaps use binary space partitioning to divide up this list for a faster load time on larger envs?
 
@@ -24,25 +26,10 @@ public class Environment {
 			for(AbstractEntity e : entities)
 			{
 				DistancedHit h = e.hitScan(x1,y1,x2,y2);
-				if(h.getDistance() > closest.getDistance())
+				if(h.getDistance() < closest.getDistance())
 					closest = h;
 			}
 			return closest;
-		}
-		public double DistanceMeasure(double x1, double y1, double x2, double y2)
-		{
-			double closest = Math.POSITIVE_INFINITY;
-			for(AbstractEntity e : entities)
-			{
-				DistancedHit h = e.hitScan(x1,y1,x2,y2);
-				if(h.hadContact())
-					closest = Math.min(closest, h.getDistance());
-			}
-			return closest;
-		}
-		public double DistanceMeasure(StaticPoint p1, StaticPoint p2)
-		{
-			return DistanceMeasure(p1.getX(), p1.getY(), p2.getX(), p2.getY());
 		}
 
 		public void draw(Graphics2D g)
@@ -53,8 +40,41 @@ public class Environment {
 			}
 		}
 
-		public static void main(String[] args)
+		public static void main(String[] args) throws InterruptedException
 		{
-			Environment env = new Environment()
+			SimpleDisplay d = new SimpleDisplay(550,400,true, true);
+			Graphics2D g = d.getGraphics2D();
+			
+			Environment env = new Environment();
+			env.entities.add(new OuterBoxBoundedEntity(20,20,510, 360));
+			env.entities.add(new BoxBoundedEntity(100, 300, 30, 80));
+			env.entities.add(new CircleBoundedEntity(300, 150, 70));
+			env.entities.add(new OuterCircleBoundedEntity(200,200,300));
+			env.entities.add(new BoxBoundedEntity(460, 126, 200, 100));
+			
+			double number = Math.random() * 20;
+			for(int i = 0; i<number; i++)
+				env.entities.add(new BoxBoundedEntity(Math.random() * d.getWidth(), Math.random() * d.getHeight(), Math.random() * 200, Math.random() * 200));
+			
+			env.draw(g);
+			d.repaint();
+			g.setColor(Color.RED);
+			
+			double scanDist = 500;
+			
+			while(true)
+			{
+				Thread.sleep(10);
+				//int c = (int)(Math.random() * 200);
+				//g.setColor(new Color(c,c,c));
+				double startx = Math.random() * d.getWidth();
+				double starty = Math.random() * d.getHeight();
+				double angle = Math.random() * Math.PI * 2 - Math.PI;
+				double endy = starty + Math.sin(angle) * scanDist;
+				double endx = startx + Math.cos(angle) * scanDist;
+				DistancedHit out = env.hitScan(startx, starty, endx, endy);
+				g.drawLine((int)startx, (int)starty, (int)out.getX(), (int)out.getY());
+				d.repaint();
+			}
 		}
 }
