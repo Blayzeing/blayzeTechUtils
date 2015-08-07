515afa0db2ad0c9ad40add2186e6bb1b66198201
aca14bfm <bfmillward1@googlemail.com>
2015-08-07 18:26:04 +0100

    Added rotate and lineSegmentIntersect methods to MoarMath.

--- math/MoarMath.java
+++ math/MoarMath.java
@@ -31,23 +31,49 @@ public class MoarMath
 		return (Math.acos(dot/(Math.hypot(x1,y1)*Math.hypot(x2,y2))));
 	}
 	/**
+	 * Rotates a given point about the origin.
+	 * @param	a	the angle to rotate the point by.
+	 */
+	 public static Point rotate(Point p, double a)
+	{
+		return(new Point(p.getX() * Math.cos(a) + p.getY() * -1 * Math.sin(a), p.getX() * Math.sin(a) + p.getY() * Math.cos(a)));
+	}
+	/**
+	 * Rotates a given point about the origin.
+	 * @param	a	the angle to rotate the point by;
+	 */
+	 public static double rotate(double x, double y, double a)
+	{
+		return(new double[]{x * Math.cos(a) + y * -1 * Math.sin(a), x * Math.sin(a) + y * Math.cos(a)});
+	}
+	/**
 	 * Calculates the intersection of two line segments.
 	 * Based on methods described here: http://stackoverflow.com/questions/563198/how-do-you-detect-where-two-line-segments-intersect
+	 * p1 -> p2 & p3 -> p4
 	 * @return	double[]	the point of intersection between the two lines, null if this does not occur. If they are overlapping, XY1 is returned.
 	*/
-	public static double[] lineSegmentIntersect(double x1, double y1, double x2, double y2, double x3, double y3, double x3, double y4)
+	public static double[] lineSegmentIntersect(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
 	{
-		NVector pmq = new NVector(new double[]{x1-x2,y1-y2});
-		NVector r = new NVector(new double[]{x2-x1, y2-y1});
-		NVector s = new NVector(new double[]{x4-x3, y4-y3});
-		double rsCross = r.getElement(0) * s.getElement(1) - r.getElement(1) * s.getElement(0);
-		double pqrCross = pmq.getElement(0) * r.getElement(1) - pmq.getElement(1) * r.getElement(0);
-		if(rsCross == 0)// Lines are parallel.
-			if(pqrCross == 0)
+		//p & q = xy1 & xy3 respectively.
+		double[] r = new double[]{x2-x1, y2-y1};
+		double[] s = new double[]{x4-x3, y4-y3};
+		double[] qMinusp = new double[]{x3-x1, y3-y1};
+		double rsCross = r[0] * s[1] - r[1] * s[0];
+		double qprCross = qMinusp[0] * r[1] - qMinusp[1] * r[0];
+		
+		if(rsCross == 0)// Some kinda parallel going on here..
+		{
+			if(qprCross == 0)// Lines are parallel & Collinear
 				return (new double[]{x1,y1});
 			else
-				return null;
-		double t = 
+				return null;// Just plain parallel, so not touching.
+		}
+		
+		double t = (qMinusp[0] * s[1] - qMinusp[1] * s[0])/rsCross;
+		double u = qprCross / rsCross;
+		if(t<= 1 && t >= 0 && u <= 1 && u >= 0)
+			return (new double[]{x1 + t*r[0], y1 + t*r[1]});
+		return null;
 	}
 	/**
 	 * Returns the intersection point of two lines defined by two points on each line; calculation
@@ -78,8 +104,18 @@ public class MoarMath
 		System.out.println("Logit of -5: " + logit(-5.0));
 		System.out.println("Logit of 0:  " + logit(0.0));
 		System.out.println("Logit of 5:  " + logit(5.0) + "\n");
-		System.out.println("Intersect point of line (-5, -1) -> (5,1) and (-5,0) -> (5,0):");
+		System.out.println("[1] Intersect point of line (-5, -1) -> (5,1) and (-5,0) -> (5,0):");
 		double[] out = lineIntersectNoSkew(-5,-1,5,1,-5,0,5,0);
 		System.out.println("("+out[0]+","+out[1]+")");
+		System.out.println("[2] Intersect point of line (-5, -1) -> (5,1) and (-5,0) -> (5,0) using line segments:");
+		out = lineSegmentIntersect(-5,-1,5,1,-5,0,5,0);
+		System.out.println("("+out[0]+","+out[1]+")");
+		out = lineSegmentIntersect(0,0,5,1,-5,0,5,0);
+		System.out.println("[3] Intersect point of line (0,0) -> (5,1) and (-5,0) -> (5,0) using line segments:");
+		System.out.println("("+out[0]+","+out[1]+")");
+		System.out.println("[4] Intersect point of line (-4,0) -> (6,-2) and (-3,0) -> (5,1) using line segments:");
+		System.out.println(lineSegmentIntersect(-4,0,6,-2,-3,0,5,1));
+		System.out.println("[5] Intersect point of line (-5,0) -> (5,0) and (-5,-1) -> (5,-1) using line segments:");
+		System.out.println( lineSegmentIntersect(-5,0,5,0,-5,-1,5,-1));
 	}
 }
