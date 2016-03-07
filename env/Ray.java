43d68d510feddea55e37bc9fbc467eaefcd136e1
Blayze Millward <blayze.millward@googlemail.com>
2016-03-07 00:32:15 +0000

    Ray and RayCluster placeholders added. They need to be coded.

---
+++ env/Ray.java
@@ -0,0 +1,4 @@
+//This class will basically consist of two points, and will then have a function that takes an environment, and casts a ray between them within it
+//It will have a draw method that will draw the rays in the standard RED/BLACK/GRAY format.
+//
+//Perhaps each Ray will have it's own location and rotation?
---
+++ env/RayCluster.java
@@ -0,0 +1,2 @@
+//This class will consist of an ArrayList of Rays
+//it will have a rotation and location and a draw method
