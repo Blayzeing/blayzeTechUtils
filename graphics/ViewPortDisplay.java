6fdc0ad5227332c33b1e9c6bbdbd8922affa1043
Blayze Millward <blayze.millward@googlemail.com>
2016-04-09 01:50:48 +0100

    Started work on ViewPortDisplay

---
+++ graphics/ViewPortDisplay.java
@@ -0,0 +1,35 @@
+package classes.graphics;
+
+import classes.math.Point;
+
+public class ViewPortDisplay extends SimpleDisplay {
+
+	public Point position = new Point(0,0);
+	private double scale = 1;
+
+	public ViewPortDisplay(int w, int h, String title)
+	{
+		super(w,h,title);
+	}
+	public ViewPortDisplay(int w, int h)
+	{
+		super(w,h);
+	}
+	public ViewPortDisplay()
+	{
+		super();
+	}
+	public ViewPortDisplay(int w, int h, boolean closeFlag)
+	{
+		super(w,h,closeFlag);
+	}
+	public ViewPortDisplay(int w, int h, boolean closeFlag, boolean resizeFlag)
+	{
+		super(w,h,closeFlag,resizeFlag);
+	}
+	public ViewPortDisplay(int w, int h, String title, boolean closeFlag, boolean resizeFlag)
+	{
+		super(w,h,title,closeFlag,resizeFlag);
+	}
+
+	public 
