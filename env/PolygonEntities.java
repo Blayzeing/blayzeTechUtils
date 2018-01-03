c1b7041d36a6dc29b3930cc8f9b2534ec22b6773
Blayze Millward <blayze.millward@googlemail.com>
2018-01-03 02:42:58 +0000

    Now can spawn polygons into previously made shapes, this is much more usable than what it was.

--- env/PolygonEntities.java
+++ env/PolygonEntities.java
@@ -15,4 +15,12 @@ public class PolygonEntities {
 		return(new PolygonEntity(0,0,new StaticPoint[]{new StaticPoint(0-offsetX, 0-offsetY), new StaticPoint(width-offsetX, 0-offsetY),
 					   		       new StaticPoint(width-offsetX, height-offsetY), new StaticPoint(0-offsetX, height-offsetY)}));
 	}
+
+	public static void addRectangleTo(PolygonEntity e, double width, double height, boolean center)
+	{
+		double offsetX = center?width/2:0;
+		double offsetY = center?height/2:0;
+		e.addPoints(new StaticPoint[]{new StaticPoint(0-offsetX, 0-offsetY), new StaticPoint(width-offsetX, 0-offsetY),
+					      new StaticPoint(width-offsetX, height-offsetY), new StaticPoint(0-offsetX, height-offsetY)});
+	}
 }
