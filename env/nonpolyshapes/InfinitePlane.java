b2dde14938cdddf33716d30baa6173def3a3c768
Blayze Millward <blayze@blayze.tech>
2024-04-04 01:57:16 +0100

    Adds infinite plane

---
+++ env/nonpolyshapes/InfinitePlane.java
@@ -0,0 +1,10 @@
+package blayzeTechUtils.env.nonpolyshapes;
+
+import blayzeTechUtils.env.AbstractEntity;
+import blayzeTechUtils.env.DistancedHit;
+
+/**
+ * An entity that is just a single infinitely-
+
+
+public class InfinitePlaneEntity
